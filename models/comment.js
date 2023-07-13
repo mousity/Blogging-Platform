9edcd614de6f23751bd422fc986c513d03e3a7ea
Samr Mouna <samr.mouna@gmail.com>
2023-07-13 14:22:45 -0400

    setting us relationships (one to many)

--- models/comment.js
+++ models/comment.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsToMany(models.Post)
+      this.belongsToMany(models.User);
     }
   }
   Comment.init({
