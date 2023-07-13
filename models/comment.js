ffe4c6ff52089654ac3136e909f95ca561870cd2
Samr Mouna <samr.mouna@gmail.com>
2023-07-13 14:25:08 -0400

    turned a many-to-many into a -one-to-many

--- models/comment.js
+++ models/comment.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsToMany(models.Post)
+      this.belongsToOne(models.Post)
       this.belongsToMany(models.User);
     }
   }
