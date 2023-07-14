a684c03b299d2b6e377c4ced9e9842eb762590d1
Samr Mouna <samr.mouna@gmail.com>
2023-07-14 17:46:16 -0400

    seeder data

---
+++ seeders/20230714175810-demo-users.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+    await queryInterface.bulkInsert('users', [{
+      name: "User1",
+      email: "example1@user.com",
+      password: "1",
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      name: "User2",
+      email: "example2@user.com",
+      password: "2",
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      name: "User3",
+      email: "example3@user.com",
+      password: "3",
+      created_at: new Date(),
+      updated_at: new Date(),
+    }], {});
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
+  }
+};
