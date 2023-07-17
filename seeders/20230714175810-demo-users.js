c3d4b284ef68cdc314496653eae386a116be9146
Samr Mouna <samr.mouna@gmail.com>
2023-07-16 22:04:42 -0400

    committing as example

--- seeders/20230714175810-demo-users.js
+++ seeders/20230714175810-demo-users.js
@@ -1,26 +1,26 @@
 'use strict';
-
+const bcrypt = require("bcryptjs");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('users', [{
       name: "User1",
       email: "example1@user.com",
-      password: "1",
+      password: await bcrypt.hash("password", 10),
       created_at: new Date(),
       updated_at: new Date(),
     },
     {
       name: "User2",
       email: "example2@user.com",
-      password: "2",
+      password: await bcrypt.hash("password", 10),
       created_at: new Date(),
       updated_at: new Date(),
     },
     {
       name: "User3",
       email: "example3@user.com",
-      password: "3",
+      password: await bcrypt.hash("password", 10),
       created_at: new Date(),
       updated_at: new Date(),
     }], {});
--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const { sequelize, User } = require("./models");
 const express = require("express");
 const port = 4000;
+const bcrypt = require("bcryptjs");
 const app = express();
+const session = require('express-session');
 require('dotenv').config();
 app.use(express.json());
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 3600000 // 1 hour
+    },
+  }));
+  
+const authenticateUser = (req, res, next) => {
+    if (!req.session.userId) {
+      return res.status(401).json({ message: 'You must be logged in to view this page.' });
+    }
+    next();
+  };
 /* GET functions for all models */
 
 // Get all USERS
-app.get("/users", async (req, res) => {
+app.get("/users", authenticateUser, async (req, res) => {
 
     try {
         const users = await User.findAll();
@@ -23,7 +40,7 @@ app.get("/users", async (req, res) => {
 /* GET SPECIFIC INSTANCE OF functions for all models */
 
 // Get SPECIFIC USER
-app.delete("/user/:id", async (req, res) => {
+app.get("/user/:id", authenticateUser, async (req, res) => {
     const userId = parseInt(req.params.id, 10);
     try {
         // Destroy a recipe specified by the id
@@ -38,7 +55,7 @@ app.delete("/user/:id", async (req, res) => {
 
 /* POST functions for all models */
 
-app.post("/users", async (req, res) => {
+app.post("/users", authenticateUser, async (req, res) => {
 
     try {
         const user = await User.create(req.body);
@@ -48,6 +65,69 @@ app.post("/users", async (req, res) => {
     }
 })
 
+app.post('/signup', async (req, res) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  
+    try {
+        const user = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: hashedPassword,
+        });
+    
+        // Send a response to the client informing them that the user was successfully created
+        res.status(201).json({
+          message: "User created!",
+          user: {
+            name: user.name,
+            email: user.email,
+          },
+        });
+      } catch (error) {
+        if (error.name === "SequelizeValidationError") {
+          return res.status(422).json({ errors: error.errors.map((e) => e.message) });
+        }
+        res.status(500).json({
+          message: "Error occurred while creating user",
+          error: error,
+        });
+      }
+  });
+
+  app.post('/login', async (req, res) => {
+    try {
+      // First, find the user by their email address
+      const user = await User.findOne({ where: { email: req.body.email } });
+  
+      if (user === null) {
+        // If the user isn't found in the database, return an 'incorrect credentials' message
+        return res.status(401).json({
+          message: 'Incorrect credentials',
+        });
+      }
+  
+      // If the user is found, we then use bcrypt to check if the password in the request matches the hashed password in the database
+      bcrypt.compare(req.body.password, user.password, (error, result) => {
+        if (result) {
+          // Passwords match
+          // TODO: Create a session for this user
+          req.session.userId = user.id;
+          res.status(200).json({
+            message: 'Logged in successfully',
+            user: {
+              name: user.name,
+              email: user.email,
+            },
+          });
+        } else {
+          // Passwords don't match
+          res.status(401).json({ message: 'Incorrect credentials' });
+        }
+      });
+    } catch (error) {
+      res.status(500).json({ message: 'An error occurred during the login process' });
+    }
+  });
 /* PUT functions for all models */
 
 
@@ -68,6 +148,17 @@ app.delete("/user/:id", async (req, res) => {
         return res.status(404).send({ message: "User not found"});
     }
 })
+
+app.delete('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.sendStatus(500);
+        }
+
+        res.clearCookie('connect.sid');
+        return res.sendStatus(200);
+    });
+});
 
 app.listen(port, async () => {
     console.log(`Server running at http://localhost:${port}`);
